53a3012c2199ba5a9d28d294b4c38b27446cc81a
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-04-23 09:40:12 +0100

    day 5 - password generator easy and hard mode

--- Day5/password-generator.py
+++ Day5/password-generator.py
@@ -5,15 +5,38 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-password = ''
+
+while True:
+    try:
+        nr_letters= int(input("How many letters would you like in your password?\n")) 
+        nr_symbols = int(input(f"How many symbols would you like?\n"))
+        nr_numbers = int(input(f"How many numbers would you like?\n"))
+        type_password = int(input("What type of password would you like [1]-Easy [2]-Hard?\n"))
+        if type_password < 1 or type_password > 2:
+            print("Please, enter valid a valid number '1' or '2' for type password!")
+        else:
+            break
+    except:
+        print("Please, enter only numbers")
+
+password = []
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+for a in range(nr_letters):
+    password.append(random.choice(letters))
 
+for b in range(nr_symbols):
+    password.append(random.choice(symbols))
+
+for c in range(nr_numbers):
+    password.append(random.choice(numbers))
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+if type_password == 2:
+    random.shuffle(password)
+
+print(f"Your new password:\n {''.join(password)}")
+    
