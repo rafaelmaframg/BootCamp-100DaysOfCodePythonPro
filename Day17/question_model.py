c929ba674a6050819726f2f93160bf4cb0a64a2b
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-05-13 20:03:43 +0100

    DAY17 - Quiz Game with OOP

---
+++ Day17/question_model.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self, text, answer) -> None:
+        self.text = text
+        self.answer = answer
+
---
+++ Day17/quiz_brain.py
@@ -0,0 +1,3 @@
+#TODO asking the questions
+#TODO checking if the answer was correct
+#TODO checking if we're the end of the quiz
