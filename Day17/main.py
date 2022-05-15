3c83c8ca2908707510bfe0fae834af3aef23a4e2
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-05-15 18:24:00 +0100

    Day 17 -  All requirements done, added json data, added docstring

--- Day17/main.py
+++ Day17/main.py
@@ -1,13 +1,16 @@
 from quiz_brain import QuizBrain
 from question_model import Question
-from data import question_data
+import json
 
 question_bank = []
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+with open("data.json", "r") as r:
+    question_data = json.load(r)
+    question_data = question_data['results']
+    for question in question_data:
+        question_text = question["question"]
+        question_answer = question["correct_answer"]
+        new_question = Question(question_text, question_answer)
+        question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
 
--- Day17/quiz_brain.py
+++ Day17/quiz_brain.py
@@ -5,15 +5,31 @@ class QuizBrain:
         self.question_list = question_list
     
     def still_has_question(self):
+        """
+        Function To check if the question is the last one of list.
+
+        Returns:
+            bool: return True if the question number is less than question list
+        """
         return self.question_number < len(self.question_list)
     
     def nex_question(self):
+        """
+        Function to get the next question and call the function to check the answer
+        """
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)")
         self.check_answer(user_answer, current_question.answer)
     
     def check_answer(self, user_answer, correct_answer):
+        """
+        Function to check the answer and contabilize the score
+
+        Args:
+            user_answer (str): user answer to check
+            correct_answer (str): the correct answer
+        """
         if user_answer.lower() == correct_answer.lower():
             print("You got it right!")
             self.score += 1
@@ -23,9 +39,3 @@ class QuizBrain:
         print(f"Your current score is: {self.score}/{self.question_number}")
         print("\n")
         
-
-
-
-#TODO asking the questions
-#TODO checking if the answer was correct
-#TODO checking if we're the end of the quiz
