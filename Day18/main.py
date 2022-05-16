fb7623a0a31646131bf45714971fa78d767039d6
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-05-16 18:01:18 +0100

    Day18 -  drawning the circle

--- Day18/main.py
+++ Day18/main.py
@@ -1,28 +1,19 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-timmy =Turtle()
-timmy.color("red")
-colors = ["cornflower blue", "lime", "orchid", "gold"]
-num_sides = 5
-
-def drawn_shaper(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        timmy.forward(100)
-        timmy.right(angle)
-
-for shape_side in range(3,11):
-    timmy.color(random.choice(colors))
-    drawn_shaper(shape_side)
+timmy = t.Turtle()
+timmy.speed("fastest")
+t.colormode(255)
 
 
 
 
+for _ in range(72):
+    timmy.color(((random.randint(0,255)),(random.randint(0,255)),(random.randint(0,255))))
+    timmy.circle(100)
+    timmy.right(5)
 
 
 
-
-
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
