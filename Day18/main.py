45bdbfec9a684b4bbd28d474a350751a6b150b49
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-05-17 14:09:12 +0100

    Day18 - done

--- Day18/main.py
+++ Day18/main.py
@@ -1,19 +1,34 @@
+import colorgram
 import turtle as t
 import random
 
+colors = colorgram.extract('Dots-scaled.jpg',30)
+list_colors = []
+
+for color in colors:
+    r = color.rgb[0]
+    g = color.rgb[1]
+    b = color.rgb[2]
+    list_colors.append((r,g,b))
+
+t.colormode(255)
+list_colors = list_colors[2:]
 timmy = t.Turtle()
 timmy.speed("fastest")
-t.colormode(255)
+timmy.hideturtle()
+y = -220
+for _ in range(10):
+    timmy.penup()
+    timmy.setx(-250)    
+    timmy.sety(y)
+    timmy.pendown()
+    y += 50
+    for _ in range(10):
+        timmy.dot(20, random.choice(list_colors))
+        timmy.penup()
+        timmy.forward(50)
+        timmy.pendown()
+
+t.done()
 
 
-
-
-for _ in range(72):
-    timmy.color(((random.randint(0,255)),(random.randint(0,255)),(random.randint(0,255))))
-    timmy.circle(100)
-    timmy.right(5)
-
-
-
-screen = t.Screen()
-screen.exitonclick()
---
+++ Day18/multicirles.py
@@ -0,0 +1,16 @@
+import turtle as t
+import random
+
+timmy = t.Turtle()
+timmy.speed("fastest")
+t.colormode(255)
+
+
+for _ in range(72):
+    timmy.color(((random.randint(0,255)),(random.randint(0,255)),(random.randint(0,255))))
+    timmy.circle(100)
+    timmy.right(5)
+
+
+screen = t.Screen()
+screen.exitonclick()
