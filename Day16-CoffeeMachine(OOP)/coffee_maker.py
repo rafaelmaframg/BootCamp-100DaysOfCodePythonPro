1fa9aba2c9bd5dd278cbffc41ca072ea1746f812
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-05-26 18:03:57 +0100

    Day 16 - Updated he docstrings

--- Day16-CoffeeMachine(OOP)/coffee_maker.py
+++ Day16-CoffeeMachine(OOP)/coffee_maker.py
@@ -1,5 +1,7 @@
 class CoffeeMaker:
-    """Models the machine that makes the coffee"""
+    """
+    Models the machine that makes the coffee
+    """
     def __init__(self):
         self.resources = {
             "water": 300,
@@ -8,13 +10,17 @@ class CoffeeMaker:
         }
 
     def report(self):
-        """Prints a report of all resources."""
+        """
+        Prints a report of all resources.
+        """
         print(f"Water: {self.resources['water']}ml")
         print(f"Milk: {self.resources['milk']}ml")
         print(f"Coffee: {self.resources['coffee']}g")
 
     def is_resource_sufficient(self, drink):
-        """Returns True when order can be made, False if ingredients are insufficient."""
+        """
+        Returns True when order can be made, False if ingredients are insufficient.
+        """
         can_make = True
         for item in drink.ingredients:
             if drink.ingredients[item] > self.resources[item]:
@@ -23,7 +29,9 @@ class CoffeeMaker:
         return can_make
 
     def make_coffee(self, order):
-        """Deducts the required ingredients from the resources."""
+        """
+        Deducts the required ingredients from the resources.
+        """
         for item in order.ingredients:
             self.resources[item] -= order.ingredients[item]
         print(f"Here is your {order.name} ☕️. Enjoy!")
