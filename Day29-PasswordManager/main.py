db51e17141511511b30b78a7c702fb752f943308
Rafael Mafra <69654497+rafaelmaframg@users.noreply.github.com>
2022-06-27 23:02:38 +0200

    day29 - created the interface, and logic to save the file

--- Day29-PasswordManager/main.py
+++ Day29-PasswordManager/main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
+from tkinter import messagebox
 import os
 
-from pandas import wide_to_long
 
 
 path = os.path.dirname(__file__)
@@ -9,6 +9,12 @@ path = os.path.dirname(__file__)
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_data():
+    with open(os.path.join(path,'data.txt'), 'a') as file:
+        file.write(f"{entry_website.get()} | {entry_email.get()} | {entry_password.get()}\n")
+    entry_password.delete(0, last='end')
+    entry_website.delete(0, last='end')
+    messagebox.showinfo(message='Done!', title="Done!")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
@@ -25,11 +31,13 @@ lbl_website.grid(sticky='e', row=1, column=0)
 
 entry_website = tk.Entry(width=43)
 entry_website.grid( row=1, column=1, columnspan=2)
+entry_website.focus()
 
 lbl_email = tk.Label(text="Email/Username:")
 lbl_email.grid(row=2, column=0)
 
 entry_email = tk.Entry(width=43)
+entry_email.insert(0, "teste@teste.com")
 entry_email.grid(row=2, column=1, columnspan=2)
 
 lbl_password = tk.Label(text="Password:")
@@ -41,6 +49,6 @@ entry_password.grid(row=3, column=1)
 btn_generate = tk.Button(text="Generate Password")
 btn_generate.grid( row=3, column=2)
 
-btn_add = tk.Button(text="ADD", width=36)
+btn_add = tk.Button(text="ADD", width=36, command=save_data)
 btn_add.grid(row=4, column=1, columnspan=2)
 window.mainloop()
