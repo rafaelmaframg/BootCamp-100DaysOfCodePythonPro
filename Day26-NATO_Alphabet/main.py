626cf65563bcf75f1546a2138ebbbef175688b64
Rafael Mafra <rafael.mafra@joingsg.com>
2022-09-30 09:40:11 +0200

    day29-30 done

--- Day26-NATO_Alphabet/main.py
+++ Day26-NATO_Alphabet/main.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import os
 
+
+def generate_phonetic():
+    name = input('Please, enter a word:').upper()
+
+    try:
+        result = [dic_alphabet[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(result)
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
@@ -10,9 +21,7 @@ dic_alphabet = {code.letter:code.code for i,code in df_alphabet.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-name = input('Please, enter a word:').upper()
+generate_phonetic()
 
-result = [dic_alphabet[letter] for letter in name]
-print(result)
 
 
--- Day29-30-PasswordManager/main.py
+++ Day29-30-PasswordManager/main.py
@@ -1,3 +1,4 @@
+import json
 from copy import copy
 import pyperclip 
 import tkinter as tk
@@ -6,10 +7,10 @@ import random
 import os
 
 
-
 PATH = os.path.dirname(__file__)
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
 
 def generate_password():
     entry_password.delete(0, last='end')
@@ -30,57 +31,91 @@ def generate_password():
     pyperclip.copy(password)
     entry_password.insert(index=0, string=password)
 
+# ---------------------------- Search Data ------------------------------- #
+
+
+def search_data():
+    website = entry_website.get()
+    if website != "":
+        try:
+            with open(os.path.join(PATH, 'data.json'), 'r') as file:
+                loaded = json.load(file)
+                messagebox.showinfo(website, message=f"User: {loaded[website]['email']}\n Password: {loaded[website]['password']}")
+        except:
+            messagebox.showerror(message="Nothing found")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
 def save_data():
     website = entry_website.get()
     email = entry_email.get()
     password = entry_password.get()
-
+    new_data = {
+        website:{
+            "email": email,
+            "password": password
+        }
+    }
 
     if website != "" and email != "" and password != "":
-        with open(os.path.join(PATH, 'data.txt'), 'a') as file:
-            file.write(f"{website} | {email} | {password}\n")
-        entry_password.delete(0, last='end')
-        entry_website.delete(0, last='end')
-        messagebox.showinfo(message='Done!', title="Done!")
+        try:
+            with open(os.path.join(PATH, 'data.json'), 'r') as file:
+                loaded = json.load(file)
+
+        except FileNotFoundError:
+            with open(os.path.join(PATH, 'data.json'), 'w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            loaded.update(new_data)
+            with open(os.path.join(PATH, 'data.json'), 'w') as file:
+                json.dump(loaded, file, indent=4)
+        finally:
+            entry_password.delete(0, last='end')
+            entry_website.delete(0, last='end')
+            messagebox.showinfo(message='Done!', title="Done!")
     
     else:
         messagebox.showerror(message="Please, don't leave any fields empty!", title="ERROR!")
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = tk.Tk()
 window.config(pady=50, padx=50)
 window.title("My Password Manager")
 
-logo = tk.Canvas(width=150, height=200)
+logo = tk.Canvas(width=120, height=175)
 img = tk.PhotoImage(file=os.path.join(PATH, 'logo.png'))
-logo.create_image(70, 100, image=img)
-logo.grid(row=0, column=1)
+logo.create_image(55, 88, image=img)
+logo.grid(row=0, column=0, columnspan=3)
 
 lbl_website = tk.Label(text="Website:")
 lbl_website.grid(sticky='e', row=1, column=0)
 
-entry_website = tk.Entry(width=43)
-entry_website.grid( row=1, column=1, columnspan=2)
+entry_website = tk.Entry(width=20)
+entry_website.grid(sticky='w', row=1, column=1)
 entry_website.focus()
 
-lbl_email = tk.Label(text="Email/Username:")
+btn_search = tk.Button(text="Search", width=13, command=search_data)
+btn_search.grid(sticky='w', row=1, column=2)
+
+lbl_email = tk.Label(text="Username:")
 lbl_email.grid(row=2, column=0)
 
-entry_email = tk.Entry(width=43)
+entry_email = tk.Entry(width=37)
 entry_email.insert(0, "teste@teste.com")
-entry_email.grid(row=2, column=1, columnspan=2)
+entry_email.grid(sticky='w', row=2, column=1, columnspan=2)
 
 lbl_password = tk.Label(text="Password:")
-lbl_password.grid(row=3, column=0)
+lbl_password.grid(sticky='e', row=3, column=0)
 
-entry_password = tk.Entry(width=24)
-entry_password.grid(row=3, column=1)
+entry_password = tk.Entry(width=20)
+entry_password.grid(sticky='w', row=3, column=1)
 
 btn_generate = tk.Button(text="Generate Password", command=generate_password)
-btn_generate.grid( row=3, column=2)
+btn_generate.grid(sticky='w', row=3, column=2)
 
-btn_add = tk.Button(text="ADD", width=36, command=save_data)
+btn_add = tk.Button(text="ADD", width=35, command=save_data)
 btn_add.grid(row=4, column=1, columnspan=2)
 window.mainloop()
