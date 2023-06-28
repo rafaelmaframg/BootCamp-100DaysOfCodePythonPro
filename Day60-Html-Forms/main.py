ae89d8350674aa23b6ce3f1ad2d3e54476ea72f1
Rafael Mafra <rafael.mafra@joingsg.com>
2023-06-28 14:22:10 +0200

    fixed sendmail function

--- Day60-Html-Forms/main.py
+++ Day60-Html-Forms/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import smtplib
-
+import os
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c3264bccdcaa99509994").json()
 
@@ -35,6 +35,7 @@ def show_post(index):
 @app.route('/form-entry', methods=['POST'])
 def receive_data():
     data = request.form
+    send_email(to_mail=OWN_EMAIL, email=data['email'], name=data['name'], phone=data['phone'], message=data['message'])
     print(data["name"])
     print(data["email"])
     print(data["phone"])
@@ -42,12 +43,12 @@ def receive_data():
     return "<h1>Successfully sent your message</h1>"
 
 
-def send_email(msg, mail):
+def send_email(message, to_mail, email, name, phone):
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=os.getenv("user"), password=os.getenv("pass"))
-        connection.sendmail(from_addr=os.getenv('user'), to_addrs=mail, msg=f"Subject:Happy Birthday\n\n{msg}")
+        connection.sendmail(from_addr=os.getenv('user'), to_addrs=to_mail, msg=email_message")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
